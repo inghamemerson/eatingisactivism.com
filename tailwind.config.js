a9a071177702d386e8924209640c23dbb9159e15
Jamie Emerson <inghamemerson@gmail.com>
2024-04-10 15:22:22 -0700

    update fonts and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.{html,tmpl}"],
-  theme: {},
+  theme: {
+    fontFamil: {
+      sans: '"Avenir Next", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+  },
   plugins: [],
 };
